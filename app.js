ecb65faedf1c24c5542ae046759cf33ecaef717a
Anthony Burns <aburns42@gmail.com>
2022-04-16 21:38:40 -0400

    Made geocode function work with positionstack api and set up forecast function to work too.

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const request = require('request');
 const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast')
 
 // Not empty in production or testing. Empty for pushing to repo.
-const url = '';
+// const url = '';
 
-request({ url: url, json: true }, (error, response) => {
-    if (error) {
-        console.log("Unable to connect to weather service.");
-    } else if (response.body.error) {
-        console.log("Unable to find location.");
-    } else {
-        console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.');
-    }
-});
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log("Unable to connect to weather service.");
+//     } else if (response.body.error) {
+//         console.log("Unable to find location.");
+//     } else {
+//         console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.');
+//     }
+// });
 
 
 // Geocoding: address -> lat/long -> weather
@@ -21,6 +22,7 @@ request({ url: url, json: true }, (error, response) => {
 // // Not empty in production or testing. Empty for pushing to repo.
 // const geocodeURL = '';
 
+// // Instructors data is from mapbox api and so it is different from mine (positionstack).
 // request({ url: geocodeURL, json: true }, (error, response) => {
 //     if (error) {
 //         console.log("Unable to connect to location services.");
@@ -37,7 +39,12 @@ request({ url: url, json: true }, (error, response) => {
 
 
 
-geocode('Perry', (error, data) +.{
+// geocode('Boston', (error, data) => {
+//     console.log('Error', error);
+//     console.log('Data', data);
+// });
+
+forecast(44.1545, -75.7088, (error, data) => {
     console.log('Error', error);
     console.log('Data', data);
 });
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,17 +1,19 @@
 const request = require('request');
 
 const geocode = (address, callback) => {
-    const url = '' + encodeURIComponent(address) + '';
-    request({ url: geocodeURL, json: true }, (error, response) => {
+    // URL contains access token, keep empty when pushing to repo.
+    // const url = '' + encodeURIComponent(address) + '';
+
+    request({ url: url, json: true }, (error, response) => {
         if (error) {
             callback('Unable to connect to location services.', undefined);
-        } else if (response.body.features.length === 0) {
-            callback("Unable to find location.", undefined);
+        } else if (response.body.data.length === 0) {
+            callback('Unable to find location.', undefined);
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
+                latitude: response.body.data[0].latitude,
+                longitude: response.body.data[0].longitude,
+                location: response.body.data[0].label
             });
         }
     });
